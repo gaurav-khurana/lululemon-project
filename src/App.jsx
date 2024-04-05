17f3f7f457a52ea0f4535b612428c01f0dbc62de
Kristopher <Krrisantan@proton.me>
2024-04-04 19:07:01 -0700

    update to Header, NOT done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
